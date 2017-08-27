78860ace2a43599eac85d71ead99da1dbfbadc5f
Sylwander <johan@sylwander.com>
2017-08-27 18:12:23 +0200

    Added NineSliceWidget stub

---
+++ Client/UI/NineSliceWidget.js
@@ -0,0 +1,18 @@
+
+// NineSliceWidget
+//
+// UI Widget with a background built up from 9 sliced texures
+// in a 3x3 pattern. Allows building scaleable UI controls with
+// custom corner and end textures.
+/////////////////////////////////////////////////////////////////////////
+
+class NineSliceWidget extends ScreenEntity
+{
+    // Constructor
+    /////////////////////////////////////////////////////////////////////////
+
+    constructor(pos, rot)
+    {
+        super(pos, rot);
+    }
+}
